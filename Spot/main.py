3a6c3e6bfd9c9551b5753c981123e1764684535f
ka-reem <kareemalmond@gmail.com>
2024-10-18 21:57:57 -0700

    dog functionalities

--- Spot/main.py
+++ Spot/main.py
@@ -14,19 +14,21 @@ def capture_image():
     print(f"Image Dimensions: {image.shape}")
     camera_capture.release()
 
-
+    # // "image": "ghcr.io/otaberu/hackathon-spot-image:main",
 def main():
     #example of using micro and speakers
-    print("Start recording audio")
-    sample_name = "aaaa.wav"
-    cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
-    print(cmd)
-    os.system(cmd)
-    print("Playing sound")
-    os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
+    # print("Start recording audio")
+    # sample_name = "aaaa.wav"
+    # cmd = f'arecord -vv --format=cd --device={os.environ["AUDIO_INPUT_DEVICE"]} -r 48000 --duration=10 -c 1 {sample_name}'
+    # print(cmd)
+    # os.system(cmd)
+    # print("Playing sound")
+    # os.system(f"ffplay -nodisp -autoexit -loglevel quiet {sample_name}")
 
+    print("running3")
     # # Capture image
 
+    time.sleep(2)
     # Use wrapper in context manager to lease control, turn on E-Stop, power on the robot and stand up at start
     # and to return lease + sit down at the end
     with SpotController(username=SPOT_USERNAME, password=SPOT_PASSWORD, robot_ip=ROBOT_IP) as spot:
@@ -40,7 +42,16 @@ def main():
                                  sleep_after_point_reached=1)
         capture_image()
         time.sleep(3)
-
+        spot.move_head_in_points(yaws=[-0.2, 0],
+                            pitches=[-0.3, 0],
+                            rolls=[-0.4, 0],
+                            sleep_after_point_reached=1)
+        time.sleep(2)
+        spot.move_head_in_points(yaws=[0, 0.2],
+                            pitches=[0, 0.3],
+                            rolls=[0, 0.4],
+                            sleep_after_point_reached=1)
+        
         # Make Spot to move by goal_x meters forward and goal_y meters left
         spot.move_to_goal(goal_x=0.5, goal_y=0)
         time.sleep(3)
